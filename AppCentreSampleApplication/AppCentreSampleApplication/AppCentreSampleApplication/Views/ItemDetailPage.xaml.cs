ce59cae3d21b97c7a583af809b505e75a36c3d11
Elmar Fazlagic <elmar.fazlagic@AuthorityPartners.com>
2023-02-23 00:27:07 +0100

    Initial commit

---
+++ AppCentreSampleApplication/AppCentreSampleApplication/AppCentreSampleApplication/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using AppCentreSampleApplication.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace AppCentreSampleApplication.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ AppCentreSampleApplication/AppCentreSampleApplication/AppCentreSampleApplication/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using AppCentreSampleApplication.Models;
+using AppCentreSampleApplication.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppCentreSampleApplication.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
